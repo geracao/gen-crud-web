d15b301859b34ccac17ce4cb39391c731bf93ff0
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-30 21:40:43 -0300

    base modal delete, modal add
    falta o edit

--- src/scripts.js
+++ src/scripts.js
@@ -1,32 +1,39 @@
 const body = document.querySelector("body");
 const main = document.querySelector("main");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const modalDelete = document.querySelector(".modal-content-delete");
+const modalAdd = document.querySelector(".modal-content-add");
 const btnDelete = document.getElementById("btn-delete");
 const btnAdd = document.getElementById("btn-add");
 
+// Consumir API
+
+// Usar para criar linha da tabela
 const createElement = (tag, className) => {
   const element = document.createElement(tag);
   element.className = className;
   return element;
 };
 
-btnDelete.addEventListener("click", () => {
-  const modalDelete = createElement("div", "modal");
-  const modalDeleteContent = createElement("div", "modal-content");
+const cancelar = (e) => {
+  e.preventDefault();
+  modal.style.display = "none";
+  modalContent.style.display = "none";
+  modalDelete.style.display = "none";
+  modalAdd.style.display = "none";
+};
 
-  modalDeleteContent.innerHTML = `
-  <h1>Deletar Membro</h1> <button><i class="fa-solid fa-xmark fa-xl"></i></button>
-  <hr />
-  <span>Você tem certeza que deseja deletar esse(s) membro(s)?</span> <br/>
-  <small>Essa ação não poderá ser revertida.</small> <br />
-  <div>
-    <button >Cancel</button> <hr/>
-    <button class="delete">Delete</button>
-  </div>
-  `;
-  modalDelete.appendChild(modalDeleteContent);
-  body.insertBefore(modalDelete, main);
+// const adicionar = () =>  { fazer POST, e atualizarTabela() }
+// const deletar = (id) =>  { fazer DELETE com id, e atualizarTabela() }
+// const editar = (id) =>   { fazer PUT com id, e atualizarTabela() }
+
+btnDelete.addEventListener("click", () => {
+  modal.style.display = "flex";
+  modalDelete.style.display = "flex";
 });
 
 btnAdd.addEventListener("click", () => {
-  console.log("apo");
+  modal.style.display = "flex";
+  modalAdd.style.display = "flex";
 });
