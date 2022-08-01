9f6aea9c31d440c90ea03369358d3d9a6fdc5d39
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-08-01 16:41:18 -0300

    ajustes
    add: getMembros, Api funciona!
    
    try to reset tablebody

--- src/scripts.js
+++ src/scripts.js
@@ -6,128 +6,41 @@ const modalAdd = document.querySelector(".modal-content-add");
 const btnDelete = document.getElementById("btn-delete");
 const btnAdd = document.getElementById("btn-modal-add");
 
-//#region 
-// var myHeaders = new Headers();
-
-// var myInit = { method: 'GET',
-//                headers: myHeaders,
-//                mode: 'cors',
-//                cache: 'default' };
-
-// var myRequest = new Request('flowers.jpg', myInit);
-
-// fetch(myRequest)
-// .then(function(response) {
-//   return response.blob();
-// })
-// .then(function(myBlob) {
-//   var objectURL = URL.createObjectURL(myBlob);
-//   myImage.src = objectURL;
-// });
-//#endregion
-
 const url = "https://localhost:7210/api/Membro";
 
-function teste(){
+function getMembros() {
   fetch(url)
-  .then((res) => res.json())
-  .then((data) => console.log(JSON.stringify(data)))
-  .catch((error) => console.error(error))
+    .then((response) => response.json())
+    .then((data) => {
+      itens = data;
+      tbody.innerHTML = "";
+      itens.forEach((item, index) => {
+        insertItem(item, index);
+      });
+    })
+    .catch((error) => console.error(error));
 }
 
-teste()
+function insertItem(item, index) {
+  let tr = document.createElement("tr");
 
-//#region 
-// async function getUsers() {
-  //   const membros = await get(url)
-//   const nome = await get(membros.data.nome)
-//   console.log(nome);
-//   // axios
-//   //   .get(url)
-//   //   .then((response) => (console.log(JSON.stringify(response.data))))
-//   //   .catch((error) => console.error(error));
-// }
+  tr.innerHTML = `
+    <td>${item.nome}</td>
+    <td>${item.email}</td>
+    <td>${item.github}</td>
+    <td>${item.phone}</td>
+    <td>
+      <button class='nonstyle-button' onclick="editItem(${index})">
+        <i class="fa-solid fa-pen"></i>
+      </button>
+      <button class='nonstyle-button' onclick="deleteItem(${index})">
+        <i class="fa-solid fa-trash"></i>
+      </button>
+    </td>
+  `;
 
-// getUsers()
-
-
-
-// function getUser(id) {
-//   axios
-//     .get(`${url}/${id}`)
-//     .then((response) => {
-//       // userId.textContent = response.data.id;
-//       // userName.textContent = response.data.name;
-//       // userCity = response.data.city;
-//       // userAvatar.src = response.data.avatar;
-//     })
-//     .catch((error) => console.error(error));
-// }
-
-// function addNewUser(newUser) {
-//   axios
-//     .post(url, newUser)
-//     .then((response) => (alertApi.textContent = JSON.stringify(response.data)))
-//     .catch((error) => console.error(error));
-// }
-
-// function updateUser(updatedUser, id) {
-//   axios
-//     .put(`${url}/${id}`, updatedUser)
-//     .then((response) => (alertApi.textContent = JSON.stringify(response.data)))
-//     .catch((error) => console.error(error));
-// }
-
-// function deleteUser(id) {
-//   axios
-//     .delete(`${url}/${id}`)
-//     .then((response) => console.log(response))
-//     .catch((error) => console.error(error));
-// }
-
-// function loadItens() {
-//   itens = getUsers();
-//   tbody.innerHTML = "";
-//   itens.forEach((item, index) => {
-//     insertItem(item, index);
-//   });
-// }
-
-// // loadItens()
-
-// function insertItem(item, index) {
-  //   let tr = document.createElement("tr");
-  
-  //   tr.innerHTML = `
-  //     <td class="body-checkbox"><input type="checkbox" /></td>
-  //     <td>${item.nome}</td>
-  //     <td>${item.email}</td>
-  //     <td>${item.github}</td>
-  //     <td>${item.phone}</td>
-  //     <td>
-//       <button class='nonstyle-button' onclick="editItem(${index})">
-//         <i class="fa-solid fa-pen"></i>
-//       </button>
-//       <button class='nonstyle-button' onclick="deleteItem(${index})>
-//         <i class="fa-solid fa-trash"></i>
-//       </button>
-//     </td>
-//   `;
-
-//   tbody.appendChild(tr)
-// }
-
-// function editItem(index) {
-  
-// }
-
-// function deleteItem(index) {
-//   itens.splice(index, 1)
-//   deleteUser(itens)
-// }
-
-// Usar para criar linha da tabela
-//#endregion 
+  tbody.appendChild(tr);
+}
 
 const cancelar = (e) => {
   e.preventDefault();
@@ -146,3 +59,72 @@ btnAdd.addEventListener("click", () => {
   modal.style.display = "flex";
   modalAdd.style.display = "flex";
 });
+
+window.onload = () => getMembros();
+
+//#region Ainda precisa Implementar
+
+
+// function getUserById(id) {
+//   fetch(`${url}/${id}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       userName.textContent = data.name;
+//       userCity.textContent = data.city;
+//       userAvatar.src = data.avatar;
+//     })
+//     .catch((error) => console.error(error));
+// }
+function editItem(index) {
+  console.log("opa ", index);
+}
+
+function adicionar() {
+  console.log();  
+}
+
+function addUser(newUser) {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(newUser),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => (alertApi.textContent = data))
+    .catch((error) => console.error(error));
+}
+
+function updateUser(updatedUser, id) {
+  fetch(`${url}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(updatedUser),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => (alertApi.textContent = data))
+    .catch((error) => console.error(error));
+}
+
+function deleteUser(id) {
+  fetch(`${url}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => (alertApi.textContent = data))
+    .catch((error) => console.error(error));
+}
+
+// function deleteItem(index) {
+//   itens.splice(index, 1)
+//   deleteUser(itens)
+// }
+
+// Usar para criar linha da tabela
+//#endregion
